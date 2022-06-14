481bcbe5b181b07b36a90ad00813f33d8e7fe850
Ahmet F. Turhan <afaturhan@gmail.com>
2022-06-14 09:06:41 +0300

    Fixed the database(duplicate entrys) and added an endpoint to filter players by their identifiers

--- players/api.py
+++ players/api.py
@@ -1,5 +1,6 @@
 from email import message
 from typing import List, Optional
+from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI, File
 from ninja.files import UploadedFile
 from typing import List
@@ -19,6 +20,15 @@ def getPlayers(request):
 def viewPlayer(request, player_id: int):
     try:
         player = Player.objects.get(pk=player_id)
+        return 200, player
+    except Player.DoesNotExist as e:
+        return 404, {"message", "Player you are looking for does not exist."}
+
+
+@api.get("/players/identifier/{identif}", response={200: List[PlayerSchema], 404: NotFoundSchema})
+def viewPlayer(request, identif: int):
+    try:
+        player = Player.objects.filter(identifier=identif)
         return 200, player
     except Player.DoesNotExist as e:
         return 404, {"message", "Player you are looking for does not exist."}
